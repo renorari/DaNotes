f66edd239d003f9d6e1b76d3244ad41d789766b5
renorari <renorari@renorari.net>
2025-07-03 10:47:38 +0900

    🎉 start: init project

---
+++ DaNotes/DaNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaNotesApp.swift
+//  DaNotes
+//
+//  Created by Renorari on 2025/07/03.
+//
+
+import SwiftUI
+
+@main
+struct DaNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
