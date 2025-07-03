c7a17531de5b6028b9ec29155f85f6b8eacc85c3
renorari <renorari@renorari.net>
2025-07-03 11:00:31 +0900

    ✨ add: add text editor

--- DaNotes/ContentView.swift
+++ DaNotes/ContentView.swift
@@ -8,14 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        TextEditor(text: $text)
+            .padding()
+            .font(.system(size: 20))
     }
 }
 
